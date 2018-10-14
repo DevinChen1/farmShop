0e9a90315c6465a49002a7596e8f3e543d85b3d0
陈成 <chencheng1990@hotmail.com>
2018-10-15 00:00:11 +0800

    添加项目文件。

---
+++ JWTTest1/Program.cs
@@ -0,0 +1,66 @@
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using System;
+using System.Collections.Generic;
+
+namespace JWTTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var token = "";
+            var secret = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";//不要泄露
+            #region 加密
+            var payload = new Dictionary<string, object>
+{
+{ "UserId", 123 },
+{ "UserName", "admin" }
+};
+
+            IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+            token = encoder.Encode(payload, secret);
+
+            Console.WriteLine(token);
+
+
+
+
+
+            #endregion
+            #region 解密
+
+            
+            try
+            {
+                //IJsonSerializer serializer = new JsonNetSerializer();
+                IDateTimeProvider provider = new UtcDateTimeProvider();
+                IJwtValidator validator = new JwtValidator(serializer, provider);
+                //IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+                IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder);
+                var json = decoder.Decode(token, secret, verify: true);
+                Console.WriteLine($"解码{json}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Token format invalid");
+            }
+            catch (TokenExpiredException)
+            {
+                Console.WriteLine("Token has expired");
+            }
+            catch (SignatureVerificationException)
+            {
+                Console.WriteLine("Token has invalid signature");
+            }
+            #endregion
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ farmShop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace farmShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                          .AddCommandLine(args)
+                          .Build();
+            String ip = config["ip"];
+            String port = config["port"];
+            Console.WriteLine($"ip={ip},port={port}");
+            return WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .UseUrls($"http://{ip}:{port}");
+        }
+        //WebHost.CreateDefaultBuilder(args)
+        //    .UseStartup<Startup>()
+        //    .UseUrls("http://127.0.0.1:5002");
+    }
+}
