0ea99294baba956b99809a3e5162844d676b67aa
陈成 <1750453097@qq.com>
2018-10-30 11:24:01 +0800

    完善项目结构

---
+++ framShop.Core/Helper/IOHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.Xml.Serialization;
+
+namespace framShop.Core
+{
+    /// <summary>
+    /// IO帮助类
+    /// </summary>
+    public class IOHelper
+    {
+        //是否已经加载了JPEG编码解码器
+        private static bool _isloadjpegcodec = false;
+        
+      
+
+        #region  序列化
+
+        /// <summary>
+        /// XML序列化
+        /// </summary>
+        /// <param name="obj">序列对象</param>
+        /// <param name="filePath">XML文件路径</param>
+        /// <returns>是否成功</returns>
+        public static bool SerializeToXml(object obj, string filePath)
+        {
+            bool result = false;
+
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+                XmlSerializer serializer = new XmlSerializer(obj.GetType());
+                serializer.Serialize(fs, obj);
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Close();
+            }
+            return result;
+
+        }
+
+        /// <summary>
+        /// XML反序列化
+        /// </summary>
+        /// <param name="type">目标类型(Type类型)</param>
+        /// <param name="filePath">XML文件路径</param>
+        /// <returns>序列对象</returns>
+        public static object DeserializeFromXML(Type type, string filePath)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                XmlSerializer serializer = new XmlSerializer(type);
+                return serializer.Deserialize(fs);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Close();
+            }
+        }
+
+        #endregion
+
+      
+
+         
+    }
+}
