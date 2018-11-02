b01016c25641477ddb4f6393a4a386f9c4b24cbc
陈成 <1750453097@qq.com>
2018-11-02 17:13:22 +0800

    大啊

---
+++ PaymentService/PaymentDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentService
+{
+    public class PaymentDBContext : DbContext
+    {
+        public PaymentDBContext(DbContextOptions<PaymentDBContext> options)
+            : base(options)
+        {
+
+        }
+        //public DbSet<ProductInfo> ProductInfos { get; set; }
+ 
+    }
+}
